d75d45bde248b83dcf1c39aa4496184afd0e5b8d
Nattawut-M <t.nattawut2020@hotmail.com>
2025-10-05 17:11:13 +0700

    feat: implement typeorm config database

---
+++ api/src/databases/databases.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.getOrThrow<string>('MYSQL_HOST'),
+        port: configService.getOrThrow<number>('MYSQL_PORT'),
+        database: configService.getOrThrow<string>('MYSQL_DATABASE'),
+        username: configService.getOrThrow<string>('MYSQL_USERNAME'),
+        password: configService.getOrThrow<string>('MYSQL_ROOT_PASSWORD'),
+        autoLoadEntities: true,
+        synchronize: configService.getOrThrow<boolean>("MYSQL_SYNCHRONIZE"),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabasesModule {}
