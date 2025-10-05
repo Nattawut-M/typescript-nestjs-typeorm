a01fbb818e740d854c78517c46b745c8d66ff33d
Nattawut-M <t.nattawut2020@hotmail.com>
2025-10-05 17:12:53 +0700

    feat: add setting `configModule` isGlobal, databaseModule, and ItemModule

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabasesModule } from './databases/databases.module';
+import { ConfigModule } from '@nestjs/config';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [DatabasesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabasesModule,
+    ItemsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
